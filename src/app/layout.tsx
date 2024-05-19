cdd7783db0035b226930c1866ca7e561207a9812
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2024-05-18 23:55:12 -0500

    ✨ [UPDATE] google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { Layout } from "@/layouts/default";
-import "./globals.css";
 import { DrawerContextProvider } from "@/context/drawer";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Portfolio | @jsonfm",
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleAnalytics gaId="G-T04T57NDVE" />
       <DrawerContextProvider>
         <body className={`overflow-x-hidden`}>
           <Layout>{children}</Layout>
