da3d1f899bac51683aacd5c50669f7c15b0c3b25
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2024-05-27 11:11:35 -0500

    ✨ [UPDATE] exp

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -4,8 +4,8 @@ import React from "react";
 const Experience = () => {
   return (
     <>
-      <section className="py-6 md:py-12 min-h-[90vh] flex justify-center items-center">
-        <div className="container-md  md:-mt-24">
+      <section className="py-6 md:pt-12 lg:pb-24 min-h-[90vh] flex justify-center items-center">
+        <div className="container-md ">
           <div className="flex flex-col items-center gap-8 md:text-xl">
             {experiences?.map((item, index) => (
               <div key={`exp-${index}`} className="w-full max-w-[400px]">
