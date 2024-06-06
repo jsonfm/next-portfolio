ee5988ab74249e84a712207a911775a84960c92c
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2024-06-05 20:56:32 -0500

    ✨ [UPDATE] favicons

--- src/layouts/default/index.tsx
+++ src/layouts/default/index.tsx
@@ -10,6 +10,29 @@ interface Props {
 export const Layout = ({ children }: Props) => {
   return (
     <>
+      <link
+        rel="icon"
+        href="/favicons/android-chrome-192x192.png"
+        type="image/png"
+        sizes="192x192"
+      />
+      <link
+        rel="icon"
+        href="/favicons/android-chrome-512x512.png"
+        type="image/png"
+        sizes="512x512"
+      />
+      <link
+        rel="icon"
+        href="/favicons/favicon-16x16.png"
+        type="image/png"
+        sizes="16x16"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicons/apple-touch-icon.png"
+      />
       <Header />
       {/* <SocialBarContact /> */}
       <main className="overflow-x-hidden min-h-[60vh]">
